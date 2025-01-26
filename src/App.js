bd992414d0ff8b5666be710515b06eeb3f636bcc
Ryan Younan <ryanyounan@gmail.com>
2025-01-25 17:19:41 -0800

    Added map

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Search from './pages/Search';
+import Map from './pages/Map';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/search" element={<Search />} />
+        <Route path="/map" element={<Map />} />
       </Routes>
     </Router>
   );
