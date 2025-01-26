99419a9b708166f6546b3cf11e44772ac79e887f
Ryan Younan <ryanyounan@gmail.com>
2025-01-25 21:36:12 -0800

    Text changing

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,11 +17,11 @@ const Home = () => {
         <Row className="d-flex align-items-center justify-content-between">
           <Col md={12} className="content-section">
             <div className="title">
-              <h1>Everyone deserves access to basic resources</h1>
-              <h2>Get the help that you deserve: explore below!</h2>
+              <h1>Everyone deserves access to basic esources.</h1>
+              <h2>Get the Help that you Deserve: Explore Below!</h2>
               <p>
                 With climate disasters worsening and inadequate resources in times of
-                crisis, many victims of wildfires are rendered in tough spots.
+                crisis, many victims of wildfires and natural disasters are rendered in tough spots.
               </p>
               <Button variant="primary" className="get-started-btn" onClick={handleGetStarted}>
                 Get Started
